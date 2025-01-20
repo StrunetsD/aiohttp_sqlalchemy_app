e6649f3121b645ec9760de437f69a27d79e58097
StrunetsD <d.strunetss@gmail.com>
2025-01-20 22:13:21 +0300

    create models book

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    genre = Column(String, nullable=False)
+    published_year = Column(Integer, nullable=False)
